234e64e1408732fac71c5bb1120b0ccbc8c8e988
duypkc12 <85305847+duypkc12@users.noreply.github.com>
2021-06-08 23:57:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Iu anh <33',
+    btnNo: 'Không, iu anhhh :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://vn.images.search.yahoo.com/search/images;_ylt=AwrxgKAbBLlgTAoAWnhrUwx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3BpdnM-?p=hinh+nen+cute&fr2=piv-web&fr=mcafee#id=2&iurl=https%3A%2F%2Fwikicachlam.com%2Fwp-content%2Fuploads%2F2019%2F10%2Fhinh-nen-cute-1.jpg&action=click' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://vn.images.search.yahoo.com/search/images;_ylt=AwrwJUVToL9ggiEA329tUwx.;_ylu=c2VjA3NlYXJjaARzbGsDYnV0dG9u;_ylc=X1MDMjExNDczNzAwNQRfcgMyBGFjdG4DY2xrBGNzcmNwdmlkA3M4eFBfekV3TGpLOF8xWklYbjY2SmdKWU1UUXVNUUFBQUFEOEc3U1QEZnIDbWNhZmVlBGZyMgNzYS1ncARncHJpZAMzSHNwVTJZNVJPLjJZR3M0TEdhU3ZBBG5fc3VnZwMxMARvcmlnaW4Ddm4uaW1hZ2VzLnNlYXJjaC55YWhvby5jb20EcG9zAzQEcHFzdHIDaGluaCUyMGFuaCUyMGRlJTIwBHBxc3RybAMxMgRxc3RybAMzMQRxdWVyeQNoaW5oJTIwYW5oJTIwZGUlMjB0aHVvbmclMjBrdXRlBHRfc3RtcAMxNjIzMTcxMjAz?p=hinh+anh+de+thuong+kute&fr=mcafee&fr2=sa-gp-vn.images.search&ei=UTF-8&x=wrt#id=7&iurl=http%3A%2F%2Ffile.vforum.vn%2Fhinh%2F2016%2F08%2Fhinh-anh-pikachu-de-thuong-kute-nhat-67.jpg&action=click' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
